bded72a85d06ab68061669df187a9d770173c9e2
allyviated <asalbores7@gmail.com>
2021-06-20 16:19:26 +0800

    Working dark mode toggle

---
+++ src/components/themeToggle.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ThemeContext } from "./themeContext";
+import { Switch } from "antd";
+
+const Toggle = () => {
+  const { theme, setTheme } = React.useContext(ThemeContext);
+
+  return (
+    <div className="transition duration-500 ease-in-out rounded-full p-2">
+      <Switch
+        checkedChildren="🌤"
+        unCheckedChildren="🌙"
+        className="cursor-pointer"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
+    </div>
+  );
+};
+
+export default Toggle;
