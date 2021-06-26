1db7fac34cfde359b02ee82fceb004424506da3b
allyviated <asalbores7@gmail.com>
2021-06-26 22:36:22 +0800

    Fix task container

--- src/components/DayContainer.js
+++ src/components/DayContainer.js
@@ -2,7 +2,7 @@ import TaskContainer from "./TaskContainer/TaskContainer";
 
 const DayContainer = ({ tasksList }) => {
   return (
-    <section className="w-full flex flex-col h-screen">
+    <section className="w-full flex flex-col h-full">
       <div className="grid gap-8 xl:grid-cols-3 lg:grid-cols-2 overflow-y-auto">
         {tasksList.map((task, index) => {
           return <TaskContainer key={index} task={task} />;
