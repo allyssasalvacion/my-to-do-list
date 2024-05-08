c08af20ca6f83e92bc05faf99304b80baba5b506
Allyssa <asalbores7@gmail.com>
2024-05-08 17:01:02 +0800

    Add text

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div className="flex flex-col mx-auto font-inter w-full h-full p-8 lg:p-24">
       <div className="flex justify-between items-center mb-6 lg:mb-8">
         <h3 className="font-bold text-xl lg:text-4xl md:text-3xl sm:text-xl m-0 mr-12 text-light_primary dark:text-dark_primary">
-          Hello, Allyssa 🌼
+          Hello, Allyssa! 🌼
         </h3>
         <div className="flex gap-8 items-center">
           <div className="transition duration-500 ease-in-out rounded-full">
